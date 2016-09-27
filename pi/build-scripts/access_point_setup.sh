dc42705e16354439f4520a32c142fc6b2fa5ac53
Kenny Chen <kendog@gmail.com>
2016-09-27 02:38:58 -0700

    access point config and build

--- pi/build-scripts/access_point_setup.sh
+++ pi/build-scripts/access_point_setup.sh
@@ -1,10 +1,13 @@
 # https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/
 
+apt-get install dnsmasq hostapd
 
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
-sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
-sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
+cp ./etc /etc
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
 
 
-sudo service hostapd start  
-sudo service dnsmasq start 
+service hostapd start  
+service dnsmasq start 
