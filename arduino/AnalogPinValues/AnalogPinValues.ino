50461cff669868f96460237fca83234ec3c9436b
Kenny Chen <kendog@gmail.com>
2016-09-27 01:53:01 -0700

    demo patch

--- arduino/AnalogPinValues/AnalogPinValues.ino
+++ arduino/AnalogPinValues/AnalogPinValues.ino
@@ -1,5 +1,5 @@
 /*
-  AnalogReadSerial
+  AnalogPinValues
   Reads an analog input on pin 0, prints the result to the serial monitor.
   Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
   Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
